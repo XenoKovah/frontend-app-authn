01dbcc4370ccb2f8f45379a8ea745c6231c4ed4f
Xeno Kovah <xeno@ost.fyi>
2024-05-07 21:01:13 -0400

    Trying to get rid of the color on the login page which intersects the site name differently depending on the user's screen width

--- src/base-component/LargeLayout.jsx
+++ src/base-component/LargeLayout.jsx
@@ -12,7 +12,7 @@ const LargeLayout = () => {
 
   return (
     <div className="w-50 d-flex">
-      <div className="col-md-9 bg-primary-400">
+      <div className="col-md-9 bg-white">
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
           <Image className="logo position-absolute" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
         </Hyperlink>
